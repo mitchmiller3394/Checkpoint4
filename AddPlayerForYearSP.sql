308fa85002c0ef5cc2f323a6449b5b73711277d6
mjmille4 <mitchmiller3394@yahoo.com>
2025-08-17 17:52:05 -0500

    played_for update

--- AddPlayerForYearSP.sql
+++ AddPlayerForYearSP.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE insertPlayerForYear(
     IN in_team_id VARCHAR(100)
 )
 BEGIN
-    INSERT INTO playedfor (team_id, player_id, year)
+    INSERT INTO played_for (team_id, player_id, year)
     VALUES (in_team_id, in_pid, in_year);
 END$$
 DELIMITER ;
